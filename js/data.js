6282347009e3de625f51b0acc2aebf7b058d66c7
Semyon <gnedin.sema@mail.ru>
2023-12-07 22:39:46 +0500

    Дз с объектами

--- js/data.js
+++ js/data.js
@@ -2,4 +2,14 @@
 MIN_INTEGER = 1;
 MAX_INTEGER = 10;
 
-export { MAX_INTEGER, MIN_INTEGER };
+//time
+TIME = ['12:00', '13:00', '14:00']
+
+//type
+TYPES_VALUES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+
+//values
+FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner']
+
+PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg']
+export { MIN_INTEGER, MAX_INTEGER, TIME, TYPES_VALUES, FEATURES, PHOTOS};
--- js/getting-data.js
+++ js/getting-data.js
@@ -1,4 +1,4 @@
-import { MIN_INTEGER, MAX_INTEGER }
+import { MIN_INTEGER, MAX_INTEGER, TIME, TYPES_VALUES, FEATURES, PHOTOS } from './data.js'
 
 // Получение рандомного числа в диапазоне(min, max):
 
@@ -9,6 +9,43 @@ const getRandomInteger = (a, b) => {
   return Math.floor(result);
 };
 
+const getRandomArrayElement = (items) => items[getRandomInteger(0, items.length - 1)];
 
-const author = {
-  avatar: `img/avatars/user${getRandomInteger()}.png`;
+const getArray = (array) => {
+  const maxLength = features.length;
+  const lengthOfArray = getRandomInteger(1, maxLength);
+  const array = [];
+  
+  for(let i = 0;i < lengthOfArray;i++) {
+    const indexOfEl = getRandomInteger(0, 5);
+    const el = features[indexOfEl];
+    
+    if (!array.includes(el)) {
+      array.push(el);
+    }
+  }
+  return array;
+}
+
+const createAuthor = () => ({
+  avatar: `img/avatars/user${getRandomInteger(MIN_INTEGER, MAX_INTEGER)}.png`,
+});
+
+const createOffer = () => ({
+  title: 'Рекомендации',
+  address: location.lat,
+  price: getRandomInteger(1, 500),
+  type: getRandomArrayElement(TYPES_VALUES),
+  rooms: getRandomInteger(1,250),
+  guests: getRandomInteger(1, 1000),
+  checkin: getRandomArrayElement(TIME),
+  checkout: getRandomArrayElement(TIME),
+  features: getArray(FEATURES),
+  description: 'Помещение хорошее. Красивое и опрятное.',
+  photos: getRandomArrayElement(PHOTOS),
+});
+
+const createLocation = () => ({
+  lat: getRandomInteger(35.65000, 35.70000),
+  lng: getRandomInteger(139.70000, 139.80000),
+});
